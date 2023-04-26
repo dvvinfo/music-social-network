d0a386385f18b8b2f385ce36e724f5eadd607d8e
dvvinfo <dvvinfo@mail.ru>
2023-04-26 12:09:02 +0300

    added the Cropper components

---
+++ src/components/UI/CroppedImage.vue
@@ -0,0 +1,23 @@
+<template>
+  <label class="block uppercase tracking-wide text-xs font-bold mb-2 text-gray-900">
+    {{ label }}
+  </label>
+  <img class="rounded w-full border-solid border-4 border-green-500" :src="image" alt="" />
+</template>
+
+<script setup>
+import { toRefs } from 'vue'
+
+const props = defineProps({
+  label: {
+    type: String
+  },
+  image: {
+    type: String
+  }
+})
+
+const { label, image } = toRefs(props)
+</script>
+
+<style lang="scss" scoped></style>
--- src/views/account/EditProfile.vue
+++ src/views/account/EditProfile.vue
@@ -2,6 +2,15 @@
   <div id="EditProfile" class="container max-w-4xl mx-auto pt-20 pb-20 px-6">
     <div class="text-gray-900 text-xl">Редактировать профиль</div>
     <div class="bg-green-500 w-full h-1"></div>
+    <!-- Modal -->
+    <CropperModal
+      v-if="showModal"
+      :min-aspect-ratio-prop="{ with: 8, height: 8 }"
+      :max-aspect-ratio-prop="{ with: 8, height: 8 }"
+      @croppedImageData="setCroppedImageData"
+      @showModal="showModal = false"
+    />
+
     <div class="flex flex-wrap mt-4 mb-6">
       <div class="w-full md:w-1/2 px-3">
         <TextInput
@@ -36,11 +45,26 @@
         />
       </div>
     </div>
+
     <div class="flex flex-wrap mt-4 mb-6">
       <div class="w-full md:w-1/2 px-3">
-        <DisplayCropperButton label="Изображение профиля" btn-text="Обновить изображение профиля" />
+        <DisplayCropperButton
+          @showModal="showModal = true"
+          label="Изображение профиля"
+          btn-text="Обновить изображение профиля"
+        />
       </div>
     </div>
+
+    <div class="flex flex-wrap mt-4 mb-6">
+      <div class="w-full md:w-1/2 px-3">
+        <CroppedImage
+          label="Обрезанное изображение"
+        :image="image"
+        />
+      </div>
+    </div>
+
     <div class="flex flex-wrap mt-4 mb-6">
       <div class="w-full px-3">
         <TextArea
@@ -53,7 +77,7 @@
     </div>
     <div class="flex flex-wrap mt-8 mb-6">
       <div class="w-full px-3">
-        <SubmitFormButton btn-text="Обновить профиль"/>
+        <SubmitFormButton btn-text="Обновить профиль" />
       </div>
     </div>
   </div>
@@ -64,12 +88,22 @@ import { ref } from 'vue'
 import TextInput from '../../components/UI/TextInput.vue'
 import DisplayCropperButton from '../../components/UI/DisplayCropperButton.vue'
 import TextArea from '../../components/UI/TextArea.vue'
-import SubmitFormButton from '../../components/UI/SubmitFormButton.vue';
+import SubmitFormButton from '../../components/UI/SubmitFormButton.vue'
+import CropperModal from '../../components/UI/CropperModal.vue'
+import CroppedImage from '../../components/UI/CroppedImage.vue'
 
 const firstName = ref(null)
 const lastName = ref(null)
 const location = ref(null)
 const description = ref(null)
+const showModal = ref(false)
+// const ImageData = ref(null)
+const image = ref(null)
+
+const setCroppedImageData = (data) =>{
+  // ImageData = data
+  image.value = data.imageUrl
+}
 </script>
 
 <style lang="scss" scoped></style>
